d5ad0709b0d77469eea6d09d5cb45f5e4b984f47
Trevor Hodsdon <mrtj458@gmail.com>
2019-12-27 22:45:16 -0700

    Fixed 404 page by changing to _error

---
+++ pages/_error.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+const ErrorPage = () => {
+  return (
+    <Layout>
+      <h1 style={{ textAlign: 'center' }}>404 page not found.</h1>
+    </Layout>
+  )
+}
+
+export default ErrorPage
