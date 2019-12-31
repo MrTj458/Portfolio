658c3c4c30a318c63566618581f523d757efd941
Trevor Hodsdon <mrtj458@gmail.com>
2019-12-31 13:49:38 -0700

    Attempt to add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,21 @@ const App = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-155217560-1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-155217560-1');`,
+          }}
+        ></script>
+
         <meta name="description" content="Portfolio for Trevor Hodsdon" />
       </Head>
 
